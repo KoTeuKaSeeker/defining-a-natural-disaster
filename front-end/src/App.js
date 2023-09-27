695adf6bb16df1ad2a71bad7580b2e4e28c77c33
KoTeuKaSeeker <emaillit8@gmail.com>
2023-09-28 01:34:29 +0400

    made a header with a Github icon and a title.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,7 +3,29 @@ import './App.css';
 
 function App() {
   return (
-    <div className="app"></div>
+    <div className="app default-font">
+      <div className="container container__content">
+        <div className="container container__header">
+          <div className="container">
+            <div className="container container__github-logo-container">
+              <div className="scince-gradient-circle scince-gradient-circle__github-icon"></div>
+              <div className="scince-gradient-circle scince-gradient-circle__github-icon scince-gradient-circle_blured"></div>
+              <img class="circle-icon circle-icon__github-icon" src="images/github-mark-white.png" alt="Link to github"/>
+              </div>
+          </div>
+
+          <div className="container container__header-text">
+            <div className="container container__header-name-container">
+              <p className="background-text background-text__header-text header-text_blue-purpure-gradiented">Sentiment analysis</p>
+              <p className="background-text background-text__blured-header-text header-text_blured header-text_blue-purpure-gradiented">Sentiment analysis</p>
+            </div>
+          </div>
+
+          <div className="container">
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
